683edea0300ead39c7afabb6505e7b249978ee1f
KayKy-Eduardo <kayky002a@gmail.com>
2024-10-30 16:25:54 -0300

    treino

--- atividade002_condicionais/f.py
+++ atividade002_condicionais/f.py
@@ -8,7 +8,7 @@ ano = int(input('Insira o ano: '))
 if ano <= 0:
     print('Este não é um ano válido.')
 else:
-    if ano % 4 == 0:
+    if (ano % 4 == 0 and ano %100 != 0) or (ano % 400 == 0):
         print(f'O ano {ano} é um ano bissexto!')
     else:
         print(f'O ano {ano} não é um ano bissexto!')
--- kayky_pessoal/meus_projetos/treino.py
+++ kayky_pessoal/meus_projetos/treino.py
@@ -3,12 +3,21 @@ import os
 
 os.system('cls')
 
-salario = float(input('Insira seu salário: '))
+a = int(input('Digite o valor: '))
+b = int(input('Digite o segundo valor: '))
+c = int(input('Digite o terceiro valor: '))
 
-if salario <= 0:
-    print('Erro!')
-elif salario >= 3000:
-    imposto = salario * 0.20
-    print(f'o imposto cobrado foi de {imposto:.2f}')
+
+if a > b or a > c:
+    if b > c:
+        auxiliar = a
+        a = b
+        b = auxiliar
+    else:
+        b < c
+        auxiliar = a
+        a = c
+        c = auxiliar
+    print(f'{a},{b} e {c}')
 else:
-    print('Isento de imposto')
+    print()
