f042f51f93f1032ee8be96bf84b6cdaf3affd55d
Kayky-Eduardo <kayky002a@gmail.com>
2024-10-30 23:06:40 -0300

    alteracao

--- kayky_pessoal/meus_projetos/treino.py
+++ kayky_pessoal/meus_projetos/treino.py
@@ -9,19 +9,17 @@ c = int(input('Digite o terceiro valor: '))
 auxiliar = 0
 
 if a > b or a > c:
-    if b > c:
+    if b < c:
         auxiliar = a
         a = b
         b = auxiliar
     else:
-        b < c
         auxiliar = a
         a = c
         c = auxiliar
-    print(f'{a},{b} e {c}')
-else:
-    auxiliar = c
-    b = a
-    c = b
-    print(f'{a}, {b} e {c}')
+if b > c:
+    auxiliar = b
+    b = c
+    c = auxiliar
+print(f'{a}, {b} e {c}')
     
