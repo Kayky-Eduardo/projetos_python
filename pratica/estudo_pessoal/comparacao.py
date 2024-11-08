1cf16d4d4e0da88131a645109c1c6008e13c6bf8
Kayky-Eduardo <kayky001a@gmail.com>
2024-11-08 16:32:43 -0300

    treino

---
+++ pratica/estudo_pessoal/comparacao.py
@@ -0,0 +1,13 @@
+import os
+
+
+os.system('cls')
+
+frase1 = input('Digite uma frase: ')
+frase2 = input('Digite outra frase: ')
+
+tamanho1 = len(frase1)
+tamanho2 = len(frase2)
+
+print(f'Tamanho da primeira frase: {tamanho1}')
+print(f'Tamanho da segunda frase:{tamanho2}')
