d90800abfb82ce1cc52e43ec19fe07ffae6917be
Kayky-Eduardo <kayky002a@gmail.com>
2024-12-10 06:35:53 -0300

    commitando

---
+++ aula/aula14_dicionarios/meus_dict/basico_dict.py
@@ -0,0 +1,69 @@
+import os
+
+
+os.system('cls')
+
+dic = {
+    'nome': 'Kayky Eduardo',
+    'idade': 18,
+    'num': [1, 2, 3],
+    'cor': 'pardo'
+}
+
+# Método len com a mesma funcionalidade de listas, só que nesse caso
+# fala quantas chaves tem dentro do dicionário
+print(f'Método len: {len(dic)}\n', '-'*70)
+# Saída: 2
+
+# Método keys, volta as chaves que estão dentro do dicionário
+print(f'Método keys: {dic.keys()}\n', '-'*70)
+
+# Método values, volta os valores dentro do dicionário
+print(f'Método values: {dic.values()}\n', '-'*70)
+
+# Método items, volta o valor e as chaves dentro do dicionário
+print(f'Método items: {dic.items()}\n', '-'*70)
+
+# Método setdefault, adiciona valor se a chave não existir
+print(f'Método setfefault: {dic.setdefault('Sobrenome', 'Leão')}')
+
+# Método copy, copia o dicionário
+# Se fizer dic2 = dic tudo que você fizer com o dic2 vai ser feito com o dic
+# Porém, o método copy é uma copia rasa, se tiver algum dado 
+# de estrutura mutável dentro do dic, vai mudar ambos os dic
+dic2 = dic.copy()
+print(f'Método copy: {dic2}\n', '='*70)
+# Exemplo:
+dic2['num'][1] = 10
+print(f'Exemplo da shallow copy(dic): {dic}')
+print(f'Exemplo da shallow copy(dic2): {dic2}\n','='*70)
+# O python tem uma biblioteca chamada de copy que resolve este problema
+
+# Método get, ele busca a chave no dicionário e se não achar ele retorna none.
+# Dá pra fazer ele voltar algo que você definiu
+print(f'Método get: {dic.get('email')}')
+print(f'Método get voltando um valor definido: ')
+print(dic.get('email', 'Eu defini'), '\n', '='*70)
+
+# Método pop, apaga um item com a chave especificada, retorna o valor
+# associado a chave removida
+dic.pop('num')
+print(f'Método pop(tirei num): {dic}')
+# Outra forma de ser usado
+cor_kayky = dic.pop('cor') # A váriavel cor_kayky vai ter o valor cor guardado
+print(f'O valor recebido pela váriavel cor_kayky: {cor_kayky}')
+print(f'O dicionário n tem mais a chave nem o valor:\n{dic}')
+# Outra forma de ser usado
+inexistente = dic.pop('asd', 'Não existe')
+print(f'fazendo print da váriavel: {inexistente}\n','-'*70)
+
+# Método popitem, remove o ultimo item adicionado, dá pra fazer uma variável
+# receber valor igual no método pop
+print(f'Antes do popitem: {dic}')
+dic.popitem()
+print(f'Depos do popitem: {dic}')
+
+
+# Dá pra fazer casting e transformar o dicionário em tuplas, listas e etc.
+# ex: list(dic.keys()), list(dic.items()), list(dic.values())
+
--- pratica/estudo_pessoal/projeto_com_davi.py
+++ pratica/estudo_pessoal/projeto_com_davi.py
@@ -1,42 +1,57 @@
 import os
 
-
 os.system('cls')
 
-nomes = []
+pessoas = []
 
 while True:
-    print('1. Cadastrar cliente: ')
-    print('2. Procurar cliente: ')
-    print('3. Sair. ')
-    resposta = input('Qual opção deseja escolher: ')
-    
-    if resposta == '1':
-        nome = input('Nome: ').lower().replace(' ', '')
-        email = input('Email: ')
-        peso = float(input('Peso: '))
-        
+    print('1. Cadastrar')
+    print('2. procurar')
+    print('3. Remover')
+    print('4. mostrar cadastrados')
+    print('5. sair')
+    escolha = input('Escolha uma opção: ')
+
+    if escolha == '1':
+        nomes = input('Digite seu nome: ').lower()
+        email = input('Insira seu email: ')
+        idade = int(input('Digite sua idade: '))
+        peso = float(input('Digite seu peso: '))
+
         cliente = {
-            'Nome': nome,
-            'Email': email,
-            'Peso': peso
-        }
-        nomes.append(cliente)
-        
-    elif resposta == '2':
-        procura = input('Qual pessoa você deseja encontrar ')
-        
+            'Nomes': nomes,
+            'email': email,
+            'idade': idade,
+            'peso': peso
+            }
+        pessoas.append(cliente)
+
+    elif escolha == '2':
+        procura_ad = input('Qual pessoa você deseja encontrar ')
+        encontrou = False
+
+        for usuario in pessoas:
+            if usuario['Nomes'] == procura_ad.lower():
+                print(f'Usuario encontrado: ',usuario)
+                encontrou = True
+        if not encontrou:
+            print(f'Pessoa {procura_ad} não encontrada.\n')
+    elif escolha == '3':
+        procura_re = input('Qual pessoa você deseja remover: ')
         encontrou = False
         
-        for usuario in nomes:
-            if usuario['Nome'] == procura.lower():
-                print('Encontrou o usuario: ', usuario)
+        for nome in pessoas:
+            if nome['Nomes'] == procura_re.lower():
+                print('Usuario removido: ', nome)
+                pessoas.remove(nome)
                 encontrou = True
-                break
         if not encontrou:
-            print(f'Pessoa {procura} não encontrada.\n')
-    elif resposta == '3':
-        print('Saindo...')
+            print(f'Pessoa {procura_re} não encontrada')
+    elif escolha == '4':
+        if pessoas:
+            print('Cadastrados: ', pessoas)
+        else:
+            print('Não tem ninguém cadastrado.')
+    elif escolha == '5':
+        print('Saindo.')
         break
-    else:
-        print('Opção invalida, tente novamente!')
