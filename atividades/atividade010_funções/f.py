869b21b4d8a877d6e152c020d9a785681f9ef293
Kayky-Eduardo <kayky002a@gmail.com>
2025-01-23 16:24:53 -0300

    atividade

--- atividades/atividade010_funções/f.py
+++ atividades/atividade010_funções/f.py
@@ -7,16 +7,17 @@ os.system('cls')
 def Receber_lista(lista1, lista2):
     dicionario = {}
     dicionario[lista1[0]] = {'peso': lista1[1], 'idade': lista1[2]}
-    dicionario[lista2[0]] = {'idade': lista2[1], 'altura': lista2[2]} 
+    dicionario[lista2[0]] = {'idade': lista2[1], 'peso': lista2[2]} 
     print(f'\nDicionário:')
     for i, j in dicionario.items():
-        print(f'{i} : {j}')
+        print(f'{i}: {j}')
 
     return dicionario
 
 nome = input('Digite um nome: ')
 peso = float(input('Digite o peso: '))
 idade = int(input('Digite sua idade: '))
+
 lista1 = [nome, peso, idade]
 lista2 = ['john', 40, 1.8]
 
